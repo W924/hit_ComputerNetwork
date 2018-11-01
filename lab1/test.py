bc59067b50575642924ea6e860ab9f2c01f6ba77
1160300302 <1295536907@qq.com>
2018-11-01 22:53:43 +0800

    lab1 last modified

--- lab1/test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-a = "Wed, 06 Jul 2016 03:06:11 GMT"
-b = "Wed, 31 Oct 2018 10:21:19 GMT"
-GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
-a_ = datetime.datetime.strptime(a, GMT_FORMAT)
-b_ = datetime.datetime.strptime(b, GMT_FORMAT)
-print a_ < b_
